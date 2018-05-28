c1671aa95bd92fe9ab059bf8e45fcb19aa10c637
sbarrios710@ufl.edu <sbarrios710@ufl.edu>
2018-05-28 13:47:02 -0700

    BlackJack Project

---
+++ BlackJack/BJClasses.py
@@ -0,0 +1,28 @@
+
+class Player:
+    #hit function
+    #stop function
+    #value function
+    #checkBankroll function
+    pass
+
+class Dealer:
+    #hit, until sum > player's value or until = 21
+    #setup (takes in deck; facedown = 2nd card)
+    #reveal
+    pass
+
+class Deck:
+    #randomized/shuffled
+    #num. of cards (52)
+    #drawCard
+    pass
+
+class Card:
+    #values
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+    
+    #suits (each suit contains 13 cards- Ace through King)
+    pass
