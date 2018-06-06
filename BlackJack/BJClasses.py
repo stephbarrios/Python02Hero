8a10d35d4a7b332e49c227749e3955e0ca52d354
Stephanie Barrios <sbarrios710@ufl.edu>
2018-06-06 10:13:18 -0700

    Functions for BJ
    Dealer Turn and Bankroll draft

--- BlackJack/BJClasses.py
+++ BlackJack/BJClasses.py
@@ -9,16 +9,46 @@ class Player:
 
             if int(player_turn) == 1:
                 card_total += newCard
+                if card_total = 21:
+                    print("Player wins! :)")
+                elif card_total > 21:
+                    print("You've BUSTED :( Dealer wins")
+                    break
+                elif card_total < 21:
+                    continue
             elif int(player_turn) == 2:
                 print('You chose to stop - the dealer will now play.')
                 break
-   
+
     #value function
+        #^do we need this since we have hitOrStop func already
+
     #checkBankroll function
-    pass
+    def checkBankroll():
+        #when and where to set bankRoll
+        betAmount = input('How much do you want to bet? ')
+        while True:
+            if int(betAmount) > bankRoll:
+                print('Sorry, you cannot bet more than what is in your bankroll')
+            elif int(betAmount) <= bankRoll:
+                bankRoll -= betAmount
+                print('You bet {} tokens. You have {} token left in your bankroll.'.format(betAmount,bankRoll))
 
 class Dealer:
     #hit, until sum > player's value or until = 21
+    def dealer_turn():
+        card_total = dealer_cardOne + dealer_cardTwo
+        while True:
+            if card_total < 21: #or player.card_total < dealer.card_total
+                print("Dealer hits")
+                card_total += newCard
+                print('Your total hand is {}'.format(card_total))
+            elif card_total = 21:
+                print('21! BlackJack!')
+            elif card_total > 21:
+                print("It's a bust!! :(  Player wins!"))
+
+
     #setup (takes in deck; facedown = 2nd card)
     #reveal
     pass
